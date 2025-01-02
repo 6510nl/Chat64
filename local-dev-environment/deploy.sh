290490d0c3c4aea6e0654b55f3cceb7a522dabe5
Berry <berry@berrydejager.com>
2025-01-02 21:01:53 +0100

    Vice compiles to tools/

--- local-dev-environment/deploy.sh
+++ local-dev-environment/deploy.sh
@@ -24,7 +24,7 @@ sudo apt install -y \
     xa65 \
     autoconf \
     openjdk-17-jre
-    
+
 mkdir tools
 mkdir tools/KickAssembler
 wget https://theweb.dk/KickAssembler/KickAssembler.zip --output-document=tools/KickAssembler/KickAssembler.zip
@@ -35,11 +35,11 @@ git clone -b chat64 https://github.com/6510nl/vice-mirror tools/vice-mirror
 cd tools/vice-mirror/vice/
 ./autogen.sh
 mkdir $HOME/vice-compiled/
-./configure --prefix=$(pwd)/tools/vice-compiled/
+./configure --prefix=$(pwd)/../../../tools/vice-compiled/
 make install
 
 cd ../../..
-wget https://www.zimmers.net/anonftp/pub/cbm/firmware/computers/c64/kernal.901227-03.bin --output-document=$HOME/vice-compiled/share/vice/C64/kernal-901227-03.bin
-wget https://www.zimmers.net/anonftp/pub/cbm/firmware/computers/c64/basic.901226-01.bin --output-document=$HOME/vice-compiled/share/vice/C64/basic-901226-01.bin
-wget https://www.zimmers.net/anonftp/pub/cbm/firmware/computers/c64/characters.901225-01.bin --output-document=$HOME/vice-compiled/share/vice/C64/chargen-901225-01.bin
+wget https://www.zimmers.net/anonftp/pub/cbm/firmware/computers/c64/kernal.901227-03.bin --output-document=$(pwd)/tools/vice-compiled/share/vice/C64/kernal-901227-03.bin
+wget https://www.zimmers.net/anonftp/pub/cbm/firmware/computers/c64/basic.901226-01.bin --output-document=$(pwd)/tools/vice-compiled/share/vice/C64/basic-901226-01.bin
+wget https://www.zimmers.net/anonftp/pub/cbm/firmware/computers/c64/characters.901225-01.bin --output-document=$(pwd)/tools/vice-compiled/share/vice/C64/chargen-901225-01.bin
 
