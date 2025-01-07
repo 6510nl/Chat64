033bb1535da58d70f147e47cd601746f32db2cce
Berry <berry@berrydejager.com>
2025-01-07 16:29:19 +0100

    Update compile.sh

--- local-dev-environment/compile.sh
+++ local-dev-environment/compile.sh
@@ -2,8 +2,8 @@
 
 # Compile the bootloader from 'bootloader.asm' to the binary 'tools/cartridge.bin' file
 # Uncomment the desired bootloader routine
-#java -jar tools/KickAssembler/KickAss.jar ../Kick\ ASM/bootloader.asm -odir ../local-dev-environment/tools/
-java -jar tools/KickAssembler/KickAss.jar ../Kick\ ASM/bootloader_debug.asm -odir ../local-dev-environment/tools/
+java -jar tools/KickAssembler/KickAss.jar ../Kick\ ASM/bootloader.asm -odir ../local-dev-environment/tools/
+#java -jar tools/KickAssembler/KickAss.jar ../Kick\ ASM/bootloader_debug.asm -odir ../local-dev-environment/tools/
 
 # Start VICE its X64SC binary and connect the custom Chat64-cartridge to the USB/COM port
 
