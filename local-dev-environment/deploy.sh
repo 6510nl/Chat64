974f32c9fbe33f19e8279757885028d11607f78e
Berry <berry@berrydejager.com>
2025-01-07 12:46:47 +0100

    Update deploy.sh

--- local-dev-environment/deploy.sh
+++ local-dev-environment/deploy.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# Install prerequisited from compiling the VICE  binaries / Chat64 bootloader sources
 sudo apt update
 sudo apt install -y \
     git \
@@ -25,19 +28,23 @@ sudo apt install -y \
     autoconf \
     openjdk-17-jre
 
+# Create a volatile 'tools' directory which is included in the .gitignore file
 mkdir tools
 mkdir tools/KickAssembler
 wget https://theweb.dk/KickAssembler/KickAssembler.zip --output-document=tools/KickAssembler/KickAssembler.zip
 unzip tools/KickAssembler/KickAssembler.zip -d tools/KickAssembler/
 
+# Clone the chat64 branch from the VICE repo as it contains the custom Chat64 cartridge support.
 git clone -b chat64 https://github.com/6510nl/vice-mirror tools/vice-mirror
 
+# Compile the VICE sources to binaries into the 'tools' directory
 cd tools/vice-mirror/vice/
 ./autogen.sh
 mkdir $HOME/vice-compiled/
 ./configure --prefix=$(pwd)/../../../tools/vice-compiled/
 make install
 
+# Fetch the required kernal, basic and char ROM files can stored those into the required path in 'tools' directory
 cd ../../..
 wget https://www.zimmers.net/anonftp/pub/cbm/firmware/computers/c64/kernal.901227-03.bin --output-document=$(pwd)/tools/vice-compiled/share/vice/C64/kernal-901227-03.bin
 wget https://www.zimmers.net/anonftp/pub/cbm/firmware/computers/c64/basic.901226-01.bin --output-document=$(pwd)/tools/vice-compiled/share/vice/C64/basic-901226-01.bin
